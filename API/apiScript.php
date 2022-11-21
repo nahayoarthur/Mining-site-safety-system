46fe1598f175d16d447f87ff3a3e9ae64df248f7
nahayoarthur <100115485+nahayoarthur@users.noreply.github.com>
2022-11-21 15:23:29 +0200

    removed unsessary codes

--- API/apiScript.php
+++ API/apiScript.php
@@ -1,5 +1,4 @@
 <?php
-//connection to database
 $conn= mysqli_connect('localhost','root','','mss-system') or die('cannot connect to the DB');
 
 if(!$conn){
@@ -8,19 +7,15 @@ if(!$conn){
 else{
   echo "DB connection successfull<br/>";
 }
-
-//$temperature=$_GET['temperature']
-
-$time = time(); //adds time stamp
 $lpg = $_POST["lpg"];
 $co = $_POST["co"];
 $smoke = $_POST["smoke"];
-$time_stamp = date('d-m-y h:i:s');
-$sql = "INSERT INTO `site-1` (lpg, smoke, carbon) VALUES ('$lpg','$smoke', '$co')"; 
+$time_stamp = date('y-m-d h:i:s');
+$sql = "INSERT INTO `site-1` (lpg, smoke, carbon, time) VALUES ('$lpg','$smoke', '$co', '$time_stamp')"; 
 if ($conn->query($sql) == TRUE){
 echo "worked<br/>";
 }
 else{
-    die('Failed query:' .$sql); //Kills the connection to the database if the query fails
+    die('Failed query:' .$sql);
     }
 ?>
