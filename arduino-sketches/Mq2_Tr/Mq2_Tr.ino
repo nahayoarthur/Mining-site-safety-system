1e65a8ce1dd54e9ebf4ec55dd62cf0a7cbe731d2
nahayoarthur <100115485+nahayoarthur@users.noreply.github.com>
2022-11-23 20:20:04 +0200

    transmitter sketch

--- arduino-sketches/Mq2_Tr/Mq2_Tr.ino
+++ arduino-sketches/Mq2_Tr/Mq2_Tr.ino
@@ -2,10 +2,17 @@
   #include <Wire.h> 
   #include <RH_ASK.h>
   #include <SPI.h>
-  RH_ASK driver;
+  #include "DHT.h"
+  #define DHTPIN 2
+  #define DHTTYPE DHT11
   
+  RH_ASK driver;
+  DHT dht(DHTPIN, DHTTYPE);
+const int BUTTON = 6;
+int BUTTONstate = 0;
 int Analog_Input = A5;
 int lpg, co, smoke;
+float temp;
 int buzzer = 5;
 String t;
 
@@ -13,6 +20,9 @@ MQ2 mq2(Analog_Input);
 
 void setup(){
   Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+  pinMode(BUTTON, INPUT);
+  dht.begin();
   mq2.begin();
   pinMode(buzzer, OUTPUT);
   RH_ASK driver;
@@ -25,8 +35,32 @@ void loop(){
   lpg = mq2.readLPG();
   co = values[1];
   co = mq2.readCO();
-  //smoke = values[0];
   smoke = mq2.readSmoke();
+  float t = dht.readTemperature();
+  Serial.println("temp: ");
+  Serial.println(t);
+  BUTTONstate = digitalRead(BUTTON);
+
+   if (BUTTONstate == HIGH)
+  {
+    tone(buzzer, 1000, 200);
+    Serial.println("Panic!!!");
+  } 
+  else{
+    noTone(buzzer);
+  }
+  
+  if (t > 30.0){
+    tone(buzzer, 1000, 200);
+    uint16_t data = temp;
+    driver.send((uint8_t *)&data, sizeof(data));
+    driver.waitPacketSent();
+  }else{
+    noTone(buzzer);
+  }
+  
+  //smoke = values[0];
+
   if (lpg < 10 && co < 10 && smoke < 10)
   {
     noTone(buzzer);
@@ -54,6 +88,6 @@ void loop(){
     driver.send((uint8_t *)&data_3, sizeof(data_3));
     
     driver.waitPacketSent();
-  delay(1000);
+//  delay(1000);
   }
 }
