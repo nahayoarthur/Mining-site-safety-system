8c31be69054d6d70023c9a514a9a66b7208b9df8
nahayoarthur <100115485+nahayoarthur@users.noreply.github.com>
2022-12-14 14:50:47 +0200

    correct timezone

--- API/apiScript.php
+++ API/apiScript.php
@@ -5,17 +5,22 @@ if(!$conn){
     die("Database connection failed: ". mysqli_connect_error());
 }
 else{
-  echo "DB connection successfull<br/>";
+  echo "DB connection successfull ";
 }
 $lpg = $_POST["lpg"];
 $co = $_POST["co"];
 $smoke = $_POST["smoke"];
 $temp = $_POST["temp"];
 $panic = $_POST["panic"];
-$time_stamp = date('y-m-d h:i:s');
-$sql = "INSERT INTO `site-1` (lpg, smoke, carbon, temp, panic, time) VALUES ('$lpg','$smoke', '$co', '$temp', '$panic', '$time_stamp')"; 
+
+$tz = 'Africa/Harare';
+$timestamp = time();
+$dt = new DateTime("now", new DateTimeZone($tz)); //first argument "must" be a string
+$dt->setTimestamp($timestamp); //adjust the object to correct timestamp
+
+$sql = "INSERT INTO `site-1` (lpg, smoke, carbon, temp, panic, time) VALUES ('$lpg','$smoke', '$co', '$temp', '$panic', '$dt->format('h:i:s d-m-y');')"; 
 if ($conn->query($sql) == TRUE){
-echo "worked<br/>";
+echo "worked";
 }
 else{
     die('Failed query:' .$sql);
